e2e1e2cb80cf1da9d0b43743e6f9cbfc4ae0126e
devemindset <menwomenchat@gmail.com>
2025-06-07 16:24:48 +0200

    update success full add a loader

--- src/app/payment_success/page.tsx
+++ src/app/payment_success/page.tsx
@@ -1,9 +1,14 @@
 import { Suspense } from "react";
-import SuccessClient from "./SuccessClient";
+import dynamic from "next/dynamic";
+import BackgroundLoader from "@/components/BackgroundLoader";
+
+
+// Dynamically import the client-only SuccessPage with SSR disabled
+const SuccessClient = dynamic(() => import("./SuccessClient"), { ssr: false });
 
 export default function Page() {
   return (
-    <Suspense fallback={<div className="text-center p-8">Loading...</div>}>
+    <Suspense fallback={<BackgroundLoader />}>
       <SuccessClient />
     </Suspense>
   );
