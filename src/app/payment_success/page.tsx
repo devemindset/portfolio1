2fbe5faef07f24c6516dbca28598743cc1ec2a83
devemindset <menwomenchat@gmail.com>
2025-06-07 15:09:50 +0200

    update payment sucess page again

--- src/app/payment_success/page.tsx
+++ src/app/payment_success/page.tsx
@@ -1,6 +1,9 @@
-// ✅ Pas besoin de "use client" ici
-import SuccessClient from './SuccessClient';
+// app/payment_success/page.tsx
+import dynamic from "next/dynamic";
 
-export default function SuccessPage() {
+// 👇 Dynamically import the client-only component
+const SuccessClient = dynamic(() => import("./SuccessClient"), { ssr: false });
+
+export default function Page() {
   return <SuccessClient />;
 }
