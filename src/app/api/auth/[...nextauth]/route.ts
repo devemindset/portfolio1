ebede3f87e14f8b734bc13c26b8e35aa310e65b8
devemindset <menwomenchat@gmail.com>
2025-06-07 14:49:44 +0200

    update nextauth flow

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,40 +1,7 @@
+// src/app/api/auth/[...nextauth]/route.ts
 import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
-import type { NextAuthOptions } from "next-auth";
+import { authOptions } from "@/lib/authOptions";
 
+const handler = NextAuth(authOptions);
 
-
-export const authOptions: NextAuthOptions = {
-  providers: [
-    Google({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-
-  callbacks: {
-    async jwt({ token, account, profile }) {
-      if (account) {
-        token.accessToken = account.access_token;
-      }
-      if (profile) {
-        token.socialId = profile.sub;
-      }
-      return token;
-    },
-
-    async session({ session, token }) {
-      session.accessToken = token.accessToken as string;
-      session.socialId = token.socialId as string;
-
-      // 👉 Appel à ton backend pour récupérer username_slug
-      
-
-      return session;
-    },
-  },
-};
-
-const handler = NextAuth(authOptions); 
-export { handler as GET, handler as POST}
+export { handler as GET, handler as POST };
