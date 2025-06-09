abe90cb53132d7b637f64a5c9a1bdffbc22069cb
devemindset <menwomenchat@gmail.com>
2025-06-09 17:32:36 +0200

    some seo configuration with next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://validationflow.com',
+  generateRobotsTxt: true,
+  generateIndexSitemap: true,
+  outDir: './public',
+};
