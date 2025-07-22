72753cc3a0dec00a2c58877e2942909f3fe8c55f
devemindset <menwomenchat@gmail.com>
2025-07-22 23:48:53 +0200

    update

---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from 'tailwindcss'
+
+
+const config: Config = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // très important pour analyser tes composants
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'spin-slow': 'spin 3s linear infinite',
+        'spin-fast': 'spin 0.5s linear infinite',
+      },
+    },
+  },
+  plugins: [
+   
+  ],
+}
+
+export default config
